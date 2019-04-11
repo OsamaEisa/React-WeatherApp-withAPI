d14e2a26ebc31d0fd4b093acb0352ac96ec53e99
Osama <engosama46@yahoo.com>
2019-04-12 02:00:41 +0300

    update degree view to be celsius

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,7 @@ const Weather = (props) => {
 
                 { props.temprature && 
                     <p className='info_key'>
-                        Temprature: <span className='info_value'> {props.temprature} </span> </p>}
+                        Temprature: <span className='info_value'> {Math.floor(props.temprature - 273.15)} &#8451; </span> </p>}
                 { props.city && 
                     <p className='info_key'>
                         City: <span className='info_value'>{props.city} </span> </p>}
@@ -16,7 +16,7 @@ const Weather = (props) => {
                         Country: <span className='info_value'>{props.country} </span> </p>}
                 { props.humidity && 
                     <p className='info_key'>
-                        Humidity: <span className='info_value'>{props.humidity} </span> </p>}
+                        Humidity: <span className='info_value'>{props.humidity} % </span> </p>}
                 { props.description && 
                     <p className='info_key'>
                         Description: <span className='info_value'>{props.description} </span> </p>}
